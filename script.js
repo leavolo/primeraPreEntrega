4d21aca7e36f5251de5fa155976130307693ebb0
leandro volo <leanvolo79@gmail.com>
2023-07-04 14:13:29 -0300

    proyecto terminado

--- script.js
+++ script.js
@@ -1,117 +1,4 @@
-/*let titulo=document.getElementById("tituloPrincipal")
-titulo.innerHTML=`<h1> bienvenidos a il-volobarber </h1>`
 
-
-let tituloAbajo=document.getElementById("tituloSecundario")
-tituloAbajo.innerHTML=`<h3> reserva de turnos </h3>`
-
-const newLocal = "lunes 9:00 am";
-let horariosDisponibles = [
-  "09:00 am",
-  "10:00am",
-  " 11:00am",
-  " 12:00am",
-  " 13:00pm",
-  " 14:00pm",
-  " 15:00pm",
- 
-];
-
-let listaHorarios = document.getElementById("horarios-lista");
-
-horariosDisponibles.forEach(function(horario) {
-  let li = document.createElement("li");
-  li.textContent = horario;
-  listaHorarios.appendChild(li);
-
-  li.addEventListener("click", function() {
-    let cartel = document.createElement("div");
-    cartel.textContent = "Completa los datos de reserva:";
-
-    let inputNombre = document.createElement("input");
-    inputNombre.placeholder = "Nombre y apellido";
-    let inputTelefono = document.createElement("input");
-    inputTelefono.placeholder = "Teléfono";
-
-    let botonReservar = document.createElement("button");
-    botonReservar.textContent = "Finalizar reserva";
-    botonReservar.addEventListener("click", function() {
-      let nombre = inputNombre.value;
-      let telefono = inputTelefono.value
-      swal.fire({
-        title:`reserva confirmada`,
-        text:`te esperamos!!`,
-        timer : `2200`,
-        icon:`success`,
-        showConfirmButton :false
-      });
-  ;
-
-      // Guardar los datos en el localStorage
-      localStorage.setItem("nombre y apellido", nombre);
-      localStorage.setItem("telefono", telefono);
-
-      // Agregar el nombre al elemento de la lista
-      li.textContent = horario + " - " + nombre;
-      li.classList.add("reservado");
-
-      cartel.style.display = "none";
-    })
-   
-    cartel.appendChild(inputNombre);
-    cartel.appendChild(inputTelefono);
-    cartel.appendChild(botonReservar);
-
-    document.body.appendChild(cartel);
-  });
-});
-
-
-
-
-// AGREGAMOS IMAGENES
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Array de rutas de imágenes
-  let imagePaths = [
-    'media/degrade.jpg',
-    'media/fotocortando.jpg',
-    'media/foto3.avif',
-    'media/foto1.jpeg',
-    `media/foto2.avif`
-  ];
-
-  // Obtener los elementos <img> por su ID
-  let imageElements = [
-    document.getElementById('image1'),
-    document.getElementById('image2'),
-    document.getElementById('image3'),
-    document.getElementById('image4'),
-   document.getElementById(`image5`),
-  ];
-
-  // Asignar las rutas de imágenes a los elementos <img>
-  for (let i = 0; i < imagePaths.length; i++) {
-    let imagePath = imagePaths[i];
-    imageElements[i].src = imagePath;
-  }
-});
-
-
-
-*/   
-
-/*const newLocal = "lunes 9:00 am";
-let horariosDisponibles = [
-  "09:00 am",
-  "10:00am",
-  " 11:00am",
-  " 12:00am",
-  " 13:00pm",
-  " 14:00pm",
-  " 15:00pm",
- 
-];*/
 
 
 let titulo = document.getElementById("tituloPrincipal");
@@ -123,16 +10,16 @@ tituloAbajo.innerHTML = `<h3> reserva de turnos </h3>`;
 let listaHorarios = document.getElementById("horarios-lista");
 
 // Cargar los horarios disponibles desde una URL utilizando Fetch
-fetch("URL_DE_TUS_HORARIOS")
+fetch("./horarios.json")
   .then(response => response.json())
   .then(data => {
-    let horariosDisponibles = data.horarios; // Supongamos que la respuesta JSON tiene una propiedad 'horarios' que contiene los horarios disponibles
-
-    horariosDisponibles.forEach(function (horario) {
-      let li = document.createElement("li");
-      li.textContent = horario;
-      listaHorarios.appendChild(li);
-
+  data.forEach(function(item){
+    let horariosDisponibles= item.horario;
+    let diasDisponibles = item.dia
+    let li = document.createElement("li");
+    li.textContent=`${diasDisponibles} - ${horariosDisponibles}`
+    listaHorarios.appendChild(li);
+  
       li.addEventListener("click", function () {
         let cartel = document.createElement("div");
         cartel.textContent = "Completa los datos de reserva:";
@@ -208,17 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-fetch('horarios.json')
-  .then(response => response.json())
-  .then(data => {
-    const listaHorarios = document.getElementById('listaHorarios');
 
-    data.forEach(horario => {
-      const li = document.createElement('li');
-      li.textContent = `${horario.dia} - ${horario.horario}`;
-      listaHorarios.appendChild(li);
-});
-});
 
 
 
